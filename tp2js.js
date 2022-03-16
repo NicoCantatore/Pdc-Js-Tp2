278b6b443f5f6d5dc21ceb2e17429a5f51c56745
NicoCantatore <nicolas.cantatore@gmail.com>
2022-03-15 21:44:06 -0300

    v1

---
+++ tp2js.js
@@ -0,0 +1,25 @@
+//Declarar una variable de nombre cantidadDeVentanasEnMiCasa y asignarle la cantidad de ventanas que hay en tu casa. Mostrar el valor de la variable por la consola.
+//Declarar una variable de nombre soyHumano y asignarle el valor true (verdadero). Mostrar el valor de la variable por la consola.
+//Declarar una variable de nombre miGustoDePizzaPreferido y asignarle la variedad de pizza que más te guste usando un String (cadena de caracteres). Mostrar el valor de la variable por la consola.
+//Mostrar por la consola los siguientes valores false, 'Hola', 0 y 'Minions' usando la misma variable.
+
+var cantidadDeVentanasEnMiCasa = 4;
+console.log(cantidadDeVentanasEnMiCasa);
+
+var soyHumano = true;
+console.log(soyHumano);
+
+var miGustoDePizzaPreferido = "Muzzarella";
+console.log(miGustoDePizzaPreferido);
+
+var mutar = false;
+console.log(mutar);
+
+mutar = "Hola";
+console.log(mutar);
+
+mutar = 0;
+console.log(mutar);
+
+mutar = "Minions";
+console.log(mutar);
